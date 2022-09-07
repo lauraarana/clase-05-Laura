d521e2bf50e01580f1ff09558778a77fe8f1d60e
lauraarana <111023053+lauraarana@users.noreply.github.com>
2022-09-07 18:15:12 -0500

    Clase 5 final

--- Clase-05.R
+++ Clase-05.R
@@ -11,3 +11,38 @@ p_load(rio, # función import/export: permite leer/escribir archivos desde difer
        skimr, # funcion skim: describe un conjunto de datos
        janitor, # función tabyl: frecuencias relativas
        dplyr) # contiene conjuntos de datos.
+
+##Importar datos 
+data_csv = import(file="input/datos_censo.csv" , skip=6 , encoding="UTF-8")
+
+data_xls = import(file="input/datos_excel.xlsx" , skip=9)
+
+##Exportar datos
+export(x=data_csv, file="output/data_csv.csv")
+
+export(x=data_xls , file="output/data_excel.rds")
+
+##2.3 Convertir
+convert(in_file = "input/datos_censo.csv" , out_file="output/datos_covert.rds")
+
+##Explorar bases de datos
+data(package="dplyr")
+
+##Obtener datos
+db = as_tibble(x = starwars)
+head(x = db , n = 5) ## pinta sobre la consola las primeras 5 observaciones de "db"
+tail(db) ## pinta sobre la consola las ultimas 5 observaciones de "db"
+
+str(object = mtcars) ## obtener la estructura del objeto "mtcars"
+
+skim(db) ##Nos resume la base de datos 
+
+colnames(db)
+db = rename (.data = db, weight=mass, height_cm=height)
+
+
+
+
+
+
+                  
